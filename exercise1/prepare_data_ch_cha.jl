32a8774209cb90cfc1583706058eb9e414ab4d01
Taiqi Lian <taiqi.lian@epfl.ch>
2026-08-27 14:36:31 +0200

    Update exercise1 notebook with Taiqi feedback
    Upload the local CH-Cha notebook and rename its preparation script to prepare_data_ch_cha.jl.

--- exercise1/prepare_data_ch_cha.jl
+++ exercise1/prepare_data_ch_cha.jl
@@ -24,8 +24,8 @@ using NCDatasets: NCDataset, defGroup, defDim, defVar
     Tethys-Chloris model, organized into different categories.
 """
 function prepare_parameters(;
-  Psan::AbstractFloat = 0.254,
-  Pcla::AbstractFloat = 0.244,
+  Psan::Float64 = 0.254,
+  Pcla::Float64 = 0.244,
 )
 
   FT = Float64
@@ -344,13 +344,10 @@ function prepare_netcdf(
   input_data_path::AbstractString,
   ca_data_path::AbstractString,
   filepath::AbstractString;
-  ZR95_L::AbstractVector = [250.0],
-  Pre_frac::Number = 1.0,
+  ZR95_L::Vector{Float64} = [250.0],
+  Pre_frac::Float64 = 1.0,
 )
   FT = Float64
-
-  ZR95_L = FT.(ZR95_L)
-  Pre_frac = FT(Pre_frac)
 
   input_data = matread(input_data_path)
 
